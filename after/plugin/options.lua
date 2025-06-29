5ece5201506221daf6a9918dd699505f084eaa35
Adilson Santos <adilson.santos97@live.com>
2025-06-29 15:58:40 -0300

    Added Lockfile and untracked files
    Kickstart.nvim and Lazy.nvim recommends to have the lockfile under
    version control so multiple machines can use the exact plugin version.
    
    As the nvim folder is not tracked anymore in dotfiles repo is better to
    add here with the default config

---
+++ after/plugin/options.lua
@@ -0,0 +1,14 @@
+vim.o.colorcolumn = '80'
+vim.o.showmatch = true
+vim.o.relativenumber = true
+vim.o.textwidth = 80
+
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.incsearch = true
+vim.o.inccommand = 'split'
+
+vim.o.scrolloff = 8
